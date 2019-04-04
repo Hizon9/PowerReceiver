353bff3d0891fb8f7ffcdd98f418ac2cdeb027d2
HizOn <hizkiaono@gmail.com>
2019-04-04 14:50:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/powerreceiver/CustomReceiver.java
@@ -0,0 +1,41 @@
+package com.example.powerreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class CustomReceiver extends BroadcastReceiver {
+
+    private static final String ACTION_CUSTOM_BROADCAST = BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String intentAction = intent.getAction();
+        if (intentAction != null) {
+            String toastMessage = context.getString(R.string.unknown);
+            switch (intentAction) {
+                case Intent.ACTION_POWER_CONNECTED:
+                    toastMessage = context.getString(R.string.connect);
+                    break;
+                case Intent.ACTION_POWER_DISCONNECTED:
+                    toastMessage = context.getString(R.string.disconnect);
+                    break;
+                case ACTION_CUSTOM_BROADCAST:
+                    toastMessage = context.getString(R.string.custom_broadcast);
+                case Intent.ACTION_HEADSET_PLUG:
+                    int state = intent.getIntExtra("state", -1);
+                    switch (state) {
+                        case 0:
+                            toastMessage = "Headset is unplugged!";
+                            break;
+                        case 1:
+                            toastMessage = "Headset is plugged!";
+                            break;
+                        default:
+                    }
+            }
+            Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
